2d7bfa9bde32ee7cb60dcbc9d91cd91e966d10cc
ZhaoPeixi627 <54923817+ZhaoPeixi627@users.noreply.github.com>
2020-02-27 19:00:25 -0500

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -2,44 +2,66 @@
 
 import tweepy #https://github.com/tweepy/tweepy
 import json
-import twitter_keys
+import keys
 import os
 
 def get_tweet(User):    
     #Twitter API credentials
-    consumer_key = twitter_keys.consumer_key
-    consumer_secret = twitter_keys.consumer_secret
-    access_key = twitter_keys.access_key
-    access_secret = twitter_keys.access_secret
+    if keys.consumer_key != "":  #only excecute when there is a key
 
-    handle = User
+        consumer_key = keys.consumer_key
+        consumer_secret = keys.consumer_secret
+        access_key = keys.access_key
+        access_secret = keys.access_secret
 
-    #authorize twitter, initialize tweepy
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
-    api = tweepy.API(auth)
 
-    #initialize a list to hold all the tweepy Tweets
-    alltweets = [] 
-    os.mkdir(User+'_'+'Photo')  
+        handle = User
 
-    #make initial request for most recent tweets (200 is the maximum allowed count)
-    try:
-        new_tweets = api.user_timeline(screen_name = handle, count=10, tweet_mode="extended")  #get most recent 10 tweets from the user
-        if new_tweets == []:
-            print("This user has no tweets yet")
-        else:
-            #save most recent tweets
-            alltweets.extend(new_tweets) 
-    except tweepy.TweepError as ex:
-        if ex.reason == "Not authorized.":
-            print("This is a private account, cannot access")
-        else:
-            print("Error: ",ex.reason) 
-    res = []
-    for i in range(len(alltweets)):
-        res.append(alltweets[i].full_text)
-    return res, User
+        #authorize twitter, initialize tweepy
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_key, access_secret)
+        api = tweepy.API(auth)
+
+        #initialize a list to hold all the tweepy Tweets
+        alltweets = [] 
+
+        # os.mkdir(User+'_'+'Photo')   ### need to uncomment this before locally testing
+
+        #make initial request for most recent tweets (200 is the maximum allowed count)
+        try:
+            new_tweets = api.user_timeline(screen_name = handle, count=10, tweet_mode="extended")  #get most recent 10 tweets from the user
+            if new_tweets == []:
+                print("This user has no tweets yet")
+            else:
+                #save most recent tweets
+                alltweets.extend(new_tweets) 
+        except tweepy.TweepError as ex:
+            if ex.reason == "Not authorized.":
+                print("This is a private account, cannot access")
+            else:
+                print("Error: ",ex.reason) 
+        res = []
+        for i in range(len(alltweets)):
+            res.append(alltweets[i].full_text)
+        
+        dic = {}
+        dic[User] = res
+        jsonData = json.dumps(dic)
+        fileObject = open(User + '.json', 'w')
+        fileObject.write(jsonData)
+        fileObject.close()
+        return res, User
+
+    
+    else:
+        name = User + '.json'
+        new_dict = open(name,encoding='utf-8')
+        dictionary = json.load(new_dict)
+        for i in dictionary.values():
+            res = i
+        for j in dictionary.keys():
+            User = j
+        return res, User
     
 
 ##  test get_tweet('realDonaldTrump')  
